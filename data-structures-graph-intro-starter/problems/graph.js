2f649fe0a99c11151ce9a67d486e1f2cf7eb4118
Keith Mellea <keithmellea@gmail.com>
2021-03-03 15:35:24 -0500

    first commit

--- data-structures-graph-intro-starter/problems/graph.js
+++ data-structures-graph-intro-starter/problems/graph.js
@@ -23,10 +23,30 @@ class Graph {
 
   buildGraph(edges) {
 
+    for (let i = 0; i < edges.length; i++) {
+      let current = edges[i];
+      let first = current[0];
+      let second = current[1];
+
+      this.addEdges(first, second);
+    }
+    return this.adjList;
   }
 
   breadthFirstTraversal(startingVertex) {
-    // Code goes here ...
+    let queue = [startingVertex];
+    let array = [];
+    console.log(startingVertex);
+    while (queue.length) {
+        let current = queue[0];
+        let list = current.adjList;
+        list.forEach(element => {
+          queue.push(element);
+        });
+        array.push(current);
+        queue.shift();
+        
+      }
   }
 
   depthFirstTraversalIterative(startingVertex) {

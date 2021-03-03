a3e6d646e11c620a2ce008434c6964c8d7fac4bd
keithmellea <74005185+keithmellea@users.noreply.github.com>
2021-03-03 12:58:40 -0500

    Add files via upload

---
+++ data-structures-graph-intro-starter/problems/graph.js
@@ -0,0 +1,44 @@
+
+class Graph {
+  constructor() {
+    this.adjlist = new Object();
+  }
+
+  addVertex(vertex) {
+    // Code goes here ...
+  }
+
+  addEdges(srcValue, destValue) {
+    // Code goes here ...
+  }
+
+  buildGraph(edges) {
+    // Code goes here ...
+  }
+
+  breadthFirstTraversal(startingVertex) {
+    // Code goes here ...
+  }
+
+  depthFirstTraversalIterative(startingVertex) {
+    // Code goes here ...
+  }
+
+  depthFirstTraversalRecursive(startingVertex, visited = new Set(), vertices = []) {
+    // Code goes here ...
+  }
+
+}
+
+module.exports = {
+  Graph
+};
+
+
+
+
+
+
+
+
+
